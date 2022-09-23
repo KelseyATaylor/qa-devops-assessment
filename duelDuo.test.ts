c0e26398d33b5813187968d73f444c446a57957b
Kelsey Taylor <kelseytay94@gmail.com>
2022-09-23 17:06:48 -0600

    post assessment review

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -19,13 +19,35 @@ test("Title shows up when page loads", async () => {
 });
 
 //Test 1
-test("Clicking Draw displays div with id = choices", async () => {
-	expect(await driver.findElement(By.id("choices"))).toBeTruthy();
-	await driver.sleep(2000);
-});
+// test("Clicking Draw displays div with id = choices", async () => {
+// 	expect(await driver.findElement(By.id("choices"))).toBeTruthy();
+// 	await driver.sleep(2000);
+// });
 
 //Test 2 - "Check that add to duo dusplays the dive with id = player duo"
-test("Clicking Add to duo displays div with id = player duo", async () => {
-	expect(await driver.findElement(By.id("player-duo"))).toBeTruthy();
-	await driver.sleep(2000);
+// test("Clicking Add to duo displays div with id = player duo", async () => {
+// 	expect(await driver.findElement(By.id("player-duo"))).toBeTruthy();
+// 	await driver.sleep(2000);
+// });
+
+//Assessment Review Solutions
+
+//Test1
+test("Choices display when draw button is clicked", async () => {
+	await driver.findElement(By.id("draw")).click();
+	await driver.sleep(300);
+	const choicesDiv = await driver.findElement(By.id("choices"));
+
+	expect(choicesDiv).not.toBe("");
+});
+
+test("Selecting a box displays in our new div", async () => {
+	await driver.findElement(By.id("draw")).click();
+	await driver.sleep(300);
+	await driver.findElement(By.xpath('(//*[text()="Add to Duo)[1]')).click();
+
+	const playerDuoDiv = await driver.findElement(By.id("player-duo"));
+	const displayed = await playerDuoDiv.isDisplayed();
+
+	expect(displayed).toBe(true);
 });
--- functions.test.ts
+++ functions.test.ts
@@ -1,12 +1,44 @@
 const { shuffleArray } = require("./utils");
 
+// describe("shuffleArray should", () => {
+// 	test("Shuffle array should return an array", () => {
+// 		expect(typeof shuffleArray).toBe("array");
+// 	});
+
+// 	test("Shuffle array length", () => {
+// 		expect(shuffleArray.length).toEqual(shuffleArray.length);
+// 		// ^^ Lol this def doesn't work Kelsey. Come back to this
+// 	});
+// });
+
+//Assessment Review Solution
+
 describe("shuffleArray should", () => {
-	test("Shuffle array should return an array", () => {
-		expect(typeof shuffleArray).toBe("array");
+	test("check to see if th length is the same after runnin gthe function", () => {
+		let arr = [1, 2, 3, 4, 5];
+		let result = shuffleArray(arr);
+		expect(arr.length).toBe(result.length);
 	});
 
-	test("Shuffle array length", () => {
-		expect(shuffleArray.length).toEqual(shuffleArray.length);
-		// ^^ Lol this def doesn't work Kelsey. Come back to this
+	test("return array with same items", () => {
+		//create array
+		//run through shuffle array function
+		//check to make sure all original values are included in new array
+
+		let arr = [1, 2, 3, 4, 5];
+		let result = shuffleArray(arr);
+
+		expect(result).toEqual(expect.arrayContaining(arr));
+
+		// let myVar = true;
+
+		// for (let i = 0; i < result.length; i++) {
+		// 	if (arr.includes(result[i]) === false) {
+		// 		myVar = false;
+		// 		return;
+		// 	}
+		// }
+
+		// expect(myVar).toBe(true);
 	});
 });
